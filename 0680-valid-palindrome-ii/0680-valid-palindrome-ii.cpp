242ab63330b3b4c7605dbf1449cd1d68bc2d6262
Aryanv1118 <aryanv1118.work@gmail.com>
2026-06-23 23:28:35 +0530

    Time: 4 ms (59.07%), Space: 22.2 MB (60.07%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+     bool isPalindrome(string &s,int l, int r) {
+        while(l<r){
+                if(tolower(s[l])!=tolower(s[r])){
+                    return false;
+                }
+                l++;
+                r--;
+        }
+        return true;
+     }
+    bool validPalindrome(string s){
+        int left = 0; 
+        int right = s.size()-1;
+        while(left<right){
+            if(s[left]!=s[right]){
+                return isPalindrome(s,left+1,right) || isPalindrome(s,left,right-1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    };
