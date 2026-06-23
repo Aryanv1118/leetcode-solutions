d592f194410c72e964b6852a9d502cad9c63f27d
Aryanv1118 <aryanv1118.work@gmail.com>
2026-06-23 23:39:48 +0530

    Time: 0 ms (100%), Space: 7.8 MB (37.61%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int fib(int n) {
+        int a = 0;
+        int b = 1;
+        int fib;
+        if(n == 0){
+            return 0;
+        }
+        if(n == 1){
+            return 1;
+        }
+        for(int i = 1;i<n;i++){
+            fib = a+b;
+            a = b;
+            b = fib;
+        }
+        return fib;
+    }
+};
